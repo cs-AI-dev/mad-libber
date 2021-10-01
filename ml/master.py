91c1eeb16382b2f11de7d54b7eda0dbe6f0d15f2
cs-AI-dev <80370389+cs-AI-dev@users.noreply.github.com>
2021-10-01 11:02:45 -0500

    Update master.py

--- ml/master.py
+++ ml/master.py
@@ -1,1 +1,9 @@
 from tkinter import *
+
+master = Tk()
+master.config(bg="black")
+
+title = Label(master, text="Mad Lib Generator", fg="white", bg="black")
+title.grid(row=1, column=1)
+
+for x in range(0x1
